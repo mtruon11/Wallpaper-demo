3328e33f6ebc037f62c90f4d44c5a8fe95035d39
Minh Truong <truonganhminh94@gmail.com>
2019-01-22 14:12:32 -0500

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,7 @@
 module.exports = {
     mongoURI: 'mongodb://demo:Sonthu1969@ds249824.mlab.com:49824/wallpaper', 
-    stripeSecretKey: 'sk_test_ygzp8Ix4t0jkAjPivnO31l2i'
+    stripeSecretKey: 'sk_test_ygzp8Ix4t0jkAjPivnO31l2i',
+    facebook_api_key: '1477266772309792',
+    facebook_secrey_key: '9a83e88fe2e1a2b826512d21043d1de4',
+    callback_url: 'htpp://localhost:8080/auth/facebook/callback',
 };
