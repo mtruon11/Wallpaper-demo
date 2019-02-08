ee9c881fdb33ee069fccf16ab88ee479ecd463e4
mtruon11 <truonganhminh94@gmail.com>
2019-02-07 21:56:32 -0500

    add related products view

--- routes/home/product.js
+++ routes/home/product.js
@@ -3,16 +3,34 @@ const router = express.Router();
 
 //Load Product model
 const Product = require('../../models/Product');
+const Categories = require('../../models/Category');
+const Tag = require('../../models/Tag');
 
 //All Available Products
 router.get('/', (req, res) => {
+    
     Product.find({status: true}, (err, products) => {
         if(err){
             console.log('Error while loading products')
         } else {
-            res.status(200).render('./home/products', {
-                user: req.user,
-                products: products
+            
+            Categories.find({}, (err, categories) => {
+                if(err) {
+                    Console.log('Error while loading categories');
+                } else {
+                    Tag.find({}, (err, tags) => {
+                        if (err) {
+                            Console.log('Error while loading tags');
+                        } else {
+                            res.status(200).render('./home/products', {
+                                user: req.user,
+                                products: products,
+                                categories: categories,
+                                tags: tags
+                            })
+                        }
+                    })
+                }
             })
         }
     });
@@ -26,9 +44,13 @@ router.get('/:id', (req, res) => {
         if(err) {
             console.log('Error while loading product')
         } else {
-            res.status(200).render('./home/product-detail', {
-                user: req.user,
-                product: product
+            console.log(product.categories);
+            Product.find({categories: product.categories}, (err, products) => {
+                res.status(200).render('./home/product-detail', {
+                    user: req.user,
+                    relatedProducts: products,
+                    product: product
+                })
             })
         }
     });
